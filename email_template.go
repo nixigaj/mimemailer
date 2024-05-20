7de5076f68fa3b427f07bdacaad31c328588e49d
Erik Junsved <junsvede@gmail.com>
2024-05-20 09:07:29 +0200

    initial fixes

--- email_template.go
+++ email_template.go
@@ -12,14 +12,12 @@ Content-Type: multipart/alternative; boundary=boundary42
 Content-Type: text/plain; charset=utf-8
 Content-Transfer-Encoding: quoted-printable
 
-{{/* This is a Quoted Printable encoded text version of the message */}}
 {{ .TextQP }}
 
 --boundary42
 Content-Type: text/html; charset=utf-8
 Content-Transfer-Encoding: quoted-printable
 
-{{/* This is a Quoted Printable encoded html version of the message */}}
 {{ .HTMLQP }}
 
 --boundary42--
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/nixigaj/mimemailer
+
+go 1.20
+
+require (
+	github.com/kylelemons/godebug v1.1.0
+	jaytaylor.com/html2text v0.0.0-20230321000545-74c2419ad056
+)
+
+require (
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf // indirect
+	golang.org/x/net v0.25.0 // indirect
+)
